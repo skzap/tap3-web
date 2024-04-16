6763d7f2eb2100d1e0235687012190bbf0314775
Adrien M <adrien@d.tube>
2024-04-16 19:14:27 +0200

    improve build script to update card backgrounds and engraving

--- update.sh
+++ update.sh
@@ -3,5 +3,7 @@ rm ../tap3.me/assets/*
 rm ../tap3.me/index.html
 rm ../tap3.me/404.html
 cp ./dist/assets/* ../tap3.me/assets/
+cp ./dist/bgs/* ../tap3.me/bgs/
+cp ./dist/cards/* ../tap3.me/cards/
 cp ./dist/index.html ../tap3.me/
 cp ./dist/index.html ../tap3.me/404.html
